74c787d5143c36b7e32f7dbb2ebe55e967da8954
Erik Aguilera <Erik.Aguilera@iam.local>
2017-09-25 18:19:52 -0500

    Added jump, refactored to use physics based movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField]
+    float jumpStrength = 10;
+
+    [SerializeField]
     float movementSpeed = 1f;
+
+    [SerializeField]
+    Transform groundDetectPoint;
+
+    [SerializeField]
+    float groundDetectRadius = 0.2f;
+
+    [SerializeField]
+    LayerMask whatCountsAsGround;
+
+    private bool isOnGround;
 
     Rigidbody2D myRigidbody;
 
 	// Use this for initialization
 	void Start ()
     {
-        //this code teleports the game object to a new location
-        //transform.position = new Vector3(0,0,0);
 
         myRigidbody = GetComponent<Rigidbody2D>();
 
@@ -36,16 +49,41 @@ public class PlayerMovement : MonoBehaviour {
         //transform.Translate(0.1f * horizontalInput, 0, 0);
 
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        myRigidbody.velocity = new Vector2(horizontalInput * movementSpeed, myRigidbody.velocity.y);
-
-        Debug.Log("Horizontal input: " + horizontalInput);
 
 
-        if (Input.GetButtonDown("Jump"))
+        UpdateIsOnGround();
+        Jump();
+        Move();
+    }
+
+
+    private void UpdateIsOnGround()
+    {
+        Collider2D[] groundObjects = Physics2D.OverlapCircleAll(
+            groundDetectPoint.position, groundDetectRadius, whatCountsAsGround);
+
+        isOnGround = groundObjects.Length > 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGround)
         {
             // crappy jump logic
-            transform.Translate(0, 5, 0);
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpStrength);
+            isOnGround = false;
         }
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        myRigidbody.velocity = new Vector2(horizontalInput * movementSpeed, myRigidbody.velocity.y);
+        Debug.Log("Horizontal input: " + horizontalInput);
     }
 }
